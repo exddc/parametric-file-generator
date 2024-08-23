7f67c7930b6f36f8eb16a92ed4dc6e8f85c0ee91
Timo Weiss <timo.weiss@protonmail.ch>
2024-08-23 13:55:56 +0200

    updated model parametric settings

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,6 +21,7 @@ const Sidebar = ({ exportSTL }: { exportSTL: () => void }) => {
     } = useStore();
 
     const [selectedColumn, setSelectedColumn] = useState<number | null>(null);
+    const [selectedRow, setSelectedRow] = useState<number | null>(null);
 
     const updateGridMatrix = (newXSections: number, newYSections: number) => {
         const newGridMatrix = createGridMatrix(
@@ -42,7 +43,11 @@ const Sidebar = ({ exportSTL }: { exportSTL: () => void }) => {
         let sectionChange = value - newGridMatrix[rowIndex][colIndex][1];
 
         newGridMatrix[rowIndex][colIndex][1] = value;
-        newGridMatrix[rowIndex][colIndex + 1][1] -= sectionChange;
+        if (colIndex != newGridMatrix[rowIndex].length - 1) {
+            newGridMatrix[rowIndex][colIndex + 1][1] -= sectionChange;
+        } else {
+            newGridMatrix[rowIndex][colIndex - 1][1] -= sectionChange;
+        }
 
         setParams({ gridMatrix: newGridMatrix });
     };
@@ -56,180 +61,207 @@ const Sidebar = ({ exportSTL }: { exportSTL: () => void }) => {
                 Adjust the drawer dimensions and grid spacing using the sliders.
                 Click the download button to export the STL file.
             </p>
-            <div className="mt-8 grid grid-cols-1 gap-5">
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="width">Width: {width} mm</Label>
-                    <Slider
-                        id="width"
-                        value={[width]}
-                        max={500}
-                        min={2}
-                        step={1}
-                        onValueChange={(value) => {
-                            setParams({ width: value[0] });
-                            updateGridMatrix(xSections, ySections);
-                        }}
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>1 mm</span>
-                        <span>500 mm</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="depth">Depth: {depth} mm</Label>
-                    <Slider
-                        id="depth"
-                        value={[depth]}
-                        max={500}
-                        min={2}
-                        step={1}
-                        onValueChange={(value) => {
-                            setParams({ depth: value[0] });
-                            updateGridMatrix(xSections, ySections);
-                        }}
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>1 mm</span>
-                        <span>500 mm</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="height">Height: {height} mm</Label>
-                    <Slider
-                        id="height"
-                        value={[height]}
-                        max={500}
-                        min={2}
-                        step={1}
-                        onValueChange={(value) =>
-                            setParams({ height: value[0] })
-                        }
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>1 mm</span>
-                        <span>500 mm</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="wallThickness">
-                        Wall Thickness: {wallThickness} mm
-                    </Label>
-                    <Slider
-                        id="wallThickness"
-                        value={[wallThickness]}
-                        max={5}
-                        min={0.4}
-                        step={0.1}
-                        onValueChange={(value) =>
-                            setParams({ wallThickness: value[0] })
-                        }
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>0.4 mm</span>
-                        <span>5 mm</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="xSections">X-Sections: {xSections}</Label>
-                    <Slider
-                        id="xSections"
-                        value={[xSections]}
-                        max={10}
-                        min={2}
-                        step={1}
-                        onValueChange={(value) => {
-                            setParams({ xSections: value[0] });
-                            updateGridMatrix(value[0], ySections);
-                        }}
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>2</span>
-                        <span>10</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="ySections">Y-Sections: {ySections}</Label>
-                    <Slider
-                        id="ySections"
-                        value={[ySections]}
-                        max={10}
-                        min={2}
-                        step={1}
-                        onValueChange={(value) => {
-                            setParams({ ySections: value[0] });
-                            updateGridMatrix(xSections, value[0]);
-                        }}
-                    />
-                    <div className="flex justify-between text-xs">
-                        <span>2</span>
-                        <span>10</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                    <Label htmlFor="columnSelector">Select Column</Label>
-                    <select
-                        id="columnSelector"
-                        value={selectedColumn ?? ''}
-                        onChange={(e) =>
-                            setSelectedColumn(
-                                e.target.value !== ''
-                                    ? Number(e.target.value)
-                                    : null
-                            )
-                        }
-                    >
-                        <option value="">None</option>
-                        {Array.from({ length: ySections }, (_, index) => (
-                            <option key={index} value={index}>
-                                Column {index + 1}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                {selectedColumn !== null && (
-                    <div className="mt-4">
-                        <Label>
-                            Adjust Sections in Column {selectedColumn + 1}:
-                        </Label>
-                        {gridMatrix[selectedColumn].map((section, colIndex) => (
-                            <div key={colIndex} className="mt-2">
-                                <Label>
-                                    Section {colIndex + 1}:{' '}
-                                    {section[1].toFixed(2)} mm
-                                </Label>
-                                <Slider
-                                    value={[section[1]]}
-                                    max={depth}
-                                    min={1}
-                                    step={1}
-                                    onValueChange={(value) =>
-                                        handleSectionLengthChange(
-                                            selectedColumn,
-                                            colIndex,
-                                            value[0]
-                                        )
-                                    }
-                                />
-                                <div className="flex justify-between text-xs">
-                                    <span>1 mm</span>
-                                    <span>{depth} mm</span>
-                                </div>
+            <ScrollArea className="h-[90vh]">
+                <div className="grid grid-cols-1 gap-2 mt-8 pb-8">
+                    <h2 className="font-bold">General Parameters</h2>
+
+                    <div className="grid grid-cols-1 gap-5">
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="width">Width: {width} mm</Label>
+                            <Slider
+                                id="width"
+                                value={[width]}
+                                max={500}
+                                min={2}
+                                step={1}
+                                onValueChange={(value) => {
+                                    setParams({ width: value[0] });
+                                    updateGridMatrix(xSections, ySections);
+                                }}
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>1 mm</span>
+                                <span>500 mm</span>
                             </div>
-                        ))}
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="depth">Depth: {depth} mm</Label>
+                            <Slider
+                                id="depth"
+                                value={[depth]}
+                                max={500}
+                                min={2}
+                                step={1}
+                                onValueChange={(value) => {
+                                    setParams({ depth: value[0] });
+                                    updateGridMatrix(xSections, ySections);
+                                }}
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>1 mm</span>
+                                <span>500 mm</span>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="height">Height: {height} mm</Label>
+                            <Slider
+                                id="height"
+                                value={[height]}
+                                max={500}
+                                min={2}
+                                step={1}
+                                onValueChange={(value) =>
+                                    setParams({ height: value[0] })
+                                }
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>1 mm</span>
+                                <span>500 mm</span>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="wallThickness">
+                                Wall Thickness: {wallThickness} mm
+                            </Label>
+                            <Slider
+                                id="wallThickness"
+                                value={[wallThickness]}
+                                max={5}
+                                min={0.4}
+                                step={0.1}
+                                onValueChange={(value) =>
+                                    setParams({ wallThickness: value[0] })
+                                }
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>0.4 mm</span>
+                                <span>5 mm</span>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="xSections">
+                                Depth-Sections: {xSections}
+                            </Label>
+                            <Slider
+                                id="xSections"
+                                value={[xSections]}
+                                max={10}
+                                min={2}
+                                step={1}
+                                onValueChange={(value) => {
+                                    setParams({ xSections: value[0] });
+                                    updateGridMatrix(value[0], ySections);
+                                }}
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>2</span>
+                                <span>10</span>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="ySections">
+                                Width-Sections: {ySections}
+                            </Label>
+                            <Slider
+                                id="ySections"
+                                value={[ySections]}
+                                max={10}
+                                min={2}
+                                step={1}
+                                onValueChange={(value) => {
+                                    setParams({ ySections: value[0] });
+                                    updateGridMatrix(xSections, value[0]);
+                                }}
+                            />
+                            <div className="flex justify-between text-xs">
+                                <span>2</span>
+                                <span>10</span>
+                            </div>
+                        </div>
                     </div>
-                )}
-                <div>
-                    <Label className="block mb-2">Color</Label>
-                    <SketchPicker
-                        color={color}
-                        onChangeComplete={(color) =>
-                            setParams({ color: color.hex })
-                        }
-                        className="mx-auto"
-                    />
+                    <div className="grid grid-cols-1 gap-2 mt-8">
+                        <h2 className="font-bold">Advanced Parameters</h2>
+                        <div className="grid grid-cols-1 gap-2">
+                            <Label htmlFor="columnSelector">
+                                Select Column
+                            </Label>
+                            <select
+                                id="columnSelector"
+                                value={selectedColumn ?? ''}
+                                onChange={(e) =>
+                                    setSelectedColumn(
+                                        e.target.value !== ''
+                                            ? Number(e.target.value)
+                                            : null
+                                    )
+                                }
+                            >
+                                <option value="">None</option>
+                                {Array.from(
+                                    { length: ySections },
+                                    (_, index) => (
+                                        <option key={index} value={index}>
+                                            Column {index + 1}
+                                        </option>
+                                    )
+                                )}
+                            </select>
+
+                            {selectedColumn !== null ? (
+                                <div className="">
+                                    <Label>
+                                        Adjust Sections in Column{' '}
+                                        {selectedColumn + 1}:
+                                    </Label>
+                                    {gridMatrix[selectedColumn].map(
+                                        (section, colIndex) => (
+                                            <div
+                                                key={colIndex}
+                                                className="mt-2"
+                                            >
+                                                <Label>
+                                                    Section {colIndex + 1}:{' '}
+                                                    {section[1].toFixed(2)} mm
+                                                </Label>
+                                                <Slider
+                                                    value={[section[1]]}
+                                                    max={depth}
+                                                    min={1}
+                                                    step={1}
+                                                    onValueChange={(value) =>
+                                                        handleSectionLengthChange(
+                                                            selectedColumn,
+                                                            colIndex,
+                                                            value[0]
+                                                        )
+                                                    }
+                                                />
+                                                <div className="flex justify-between text-xs">
+                                                    <span>1 mm</span>
+                                                    <span>{depth} mm</span>
+                                                </div>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            ) : (
+                                <div className="hidden"></div>
+                            )}
+                        </div>
+                        <div className="grid grid-cols-1 gap-2 mt-2">
+                            <Label>Color</Label>
+                            <SketchPicker
+                                color={color}
+                                onChangeComplete={(color) =>
+                                    setParams({ color: color.hex })
+                                }
+                                className="mx-auto"
+                            />
+                        </div>
+                    </div>
+                    <Button onClick={exportSTL}>Download STL</Button>
                 </div>
-                <Button onClick={exportSTL}>Download STL</Button>
-            </div>
+            </ScrollArea>
         </div>
     );
 };
--- lib/createGridModel.ts
+++ lib/createGridModel.ts
@@ -16,21 +16,6 @@ export const createGridModel = (
         roughness: 1,
         metalness: 1,
     });
-    const material_red = new THREE.MeshStandardMaterial({
-        color: 0xff0000,
-        roughness: 1,
-        metalness: 1,
-    });
-    const material_green = new THREE.MeshStandardMaterial({
-        color: 0x00ff00,
-        roughness: 1,
-        metalness: 1,
-    });
-    const material_blue = new THREE.MeshStandardMaterial({
-        color: 0x0000ff,
-        roughness: 1,
-        metalness: 1,
-    });
     const lineMaterial = new THREE.LineBasicMaterial({ color: 0x606060 });
 
     const createBar = (thickness: number, length: number, height: number, positionX: number, positionY: number, rotation: number, material_type: THREE.MeshStandardMaterial) => {
@@ -42,25 +27,37 @@ export const createGridModel = (
         return mesh;
     }
 
+    const createEdge = (thickness: number, length: number, height: number, positionX: number, positionY: number, rotation: number, material_type: THREE.LineBasicMaterial) => {
+        const geometry = new THREE.BoxGeometry(thickness, height, length);
+        const mesh = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), material_type);
+        mesh.position.x = positionX;
+        mesh.position.z = positionY;
+        mesh.rotation.y = rotation;
+        return mesh;
+    }
+
     // Create outside walls
-    gridGroup.add(createBar(wallThickness, width + wallThickness, height, -depth / 2, 0, 0, material_red));
-    gridGroup.add(createBar(wallThickness, width + wallThickness, height, depth / 2, 0, 0, material_red));
-    gridGroup.add(createBar(depth + wallThickness, wallThickness, height, 0, -width/2, 0, material_blue));
-    gridGroup.add(createBar(depth + wallThickness, wallThickness, height, 0, width/2, 0, material_blue));
+    gridGroup.add(createBar(wallThickness, width + wallThickness, height, -depth / 2, 0, 0, material));
+    gridGroup.add(createBar(wallThickness, width + wallThickness, height, depth / 2, 0, 0, material));
+    gridGroup.add(createBar(depth + wallThickness, wallThickness, height, 0, -width/2, 0, material));
+    gridGroup.add(createBar(depth + wallThickness, wallThickness, height, 0, width/2, 0, material));
+
+    gridGroup.add(createEdge(wallThickness, width + wallThickness, height, -depth / 2, 0, 0, lineMaterial));
+    gridGroup.add(createEdge(wallThickness, width + wallThickness, height, depth / 2, 0, 0, lineMaterial));
+    gridGroup.add(createEdge(depth + wallThickness, wallThickness, height, 0, -width/2, 0, lineMaterial));
+    gridGroup.add(createEdge(depth + wallThickness, wallThickness, height, 0, width/2, 0, lineMaterial));
 
     // Create inside walls
     for (let i = 0; i < gridMatrix.length; i++) {
         let previousSectionsTotalWidth = 0;
         for (let j = 0; j < gridMatrix[i].length; j++) {
             const [sectionDepth, sectionWidth] = gridMatrix[i][j];
-            console.log("i: " + i + " j: " + j + " sectionWidth: " + sectionWidth + " sectionDepth: " + sectionDepth);
             
             // Get the total depth of the previous sections the same column
             let previousSectionsTotalDepth = 0;
             for(let k = 0; k < i; k++){
                 previousSectionsTotalDepth += gridMatrix[k][j][0];
             }
-            console.log(previousSectionsTotalDepth);
 
             previousSectionsTotalWidth += sectionWidth;
 
@@ -68,60 +65,17 @@ export const createGridModel = (
             let depthOffset = depth / 2 - previousSectionsTotalDepth;
             let widthOffset = width / 2 - previousSectionsTotalWidth;
 
-            console.log("depthOffset: " + depthOffset + " widthOffset: " + widthOffset);
-
             // Create depth bars (X-axis)
             if(j < gridMatrix[i].length - 1){
-                gridGroup.add(createBar(sectionDepth, wallThickness, height, depthOffset - sectionDepth/2, widthOffset, 0, material_green));
+                gridGroup.add(createBar(sectionDepth, wallThickness, height, depthOffset - sectionDepth/2, widthOffset, 0, material));
+                gridGroup.add(createEdge(sectionDepth, wallThickness, height, depthOffset - sectionDepth/2, widthOffset, 0, lineMaterial));
             }
 
             // Create width bars (Z-axis)
             if(i > 0){
                 gridGroup.add(createBar(wallThickness, sectionWidth, height, depthOffset, widthOffset + sectionWidth/2, 0, material));
+                gridGroup.add(createEdge(wallThickness, sectionWidth, height, depthOffset, widthOffset + sectionWidth/2, 0, lineMaterial));
             }
         }
     }
-
-
-
-
-
-    
-
-    
-
-    
-
-    /* for (let i = 0; i < gridMatrix.length; i++) {
-        for (let j = 0; j < gridMatrix[i].length; j++) {
-            const [sectionWidth, sectionDepth] = gridMatrix[i][j];
-
-            if(i > 0){
-                // Create vertical bars (X-axis)
-                const verticalGeometry = new THREE.BoxGeometry(wallThickness, height, sectionDepth - wallThickness);
-                const verticalBar = new THREE.Mesh(verticalGeometry, material);
-                verticalBar.position.x = sectionWidth * i;
-                verticalBar.position.z = sectionDepth * j + sectionDepth / 2;
-                gridGroup.add(verticalBar);
-
-                const verticalEdges = new THREE.EdgesGeometry(verticalGeometry);
-                const verticalLine = new THREE.LineSegments(verticalEdges, lineMaterial);
-                verticalLine.position.copy(verticalBar.position);
-                gridGroup.add(verticalLine);
-            }
-            
-
-            if(j < gridMatrix[i].length - 1){
-                // Create horizontal bars (Z-axis)
-                const horizontalGeometry = new THREE.BoxGeometry(sectionWidth - wallThickness, height, wallThickness);
-                const horizontalBar = new THREE.Mesh(horizontalGeometry, material);
-                horizontalBar.position.x = sectionWidth / 2 + sectionWidth * i;
-                horizontalBar.position.z = sectionDepth / 2 + sectionDepth * j  + sectionDepth / 2;
-                gridGroup.add(horizontalBar);
-            }
-        }
-    }
-
-    gridGroup.position.x = -width / 2;
-    gridGroup.position.z = -depth / 2; */
 };
